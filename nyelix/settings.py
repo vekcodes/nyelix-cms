eb0bdad27962c7934537ec8dd69144d1260329d2
workvek <152685157+workvek@users.noreply.github.com>
2024-10-08 18:34:53 +0545

    collectstatic

--- nyelix/settings.py
+++ nyelix/settings.py
@@ -129,6 +129,9 @@ STATIC_URL = 'static/'
 import os
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 
 Media_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
